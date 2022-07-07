92bac613e192ae41506b184fbec241952c4e350d
Jan Razny <netorg@gmx.de>
2022-07-07 22:52:57 +0200

    models

---
+++ js/game.js
@@ -0,0 +1,18 @@
+
+
+let canvas;
+let ctx;
+let character = new Character();
+let enemies = [
+    new Chicken(),
+    new Chicken(),
+    new Chicken()
+
+];
+
+function init() {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
+    console.log('MyCharacter', character)
+}

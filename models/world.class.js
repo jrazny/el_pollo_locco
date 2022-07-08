c4b3193da673a5e128e1da0e85b1919942b0cbfc
Jan Razny <netorg@gmx.de>
2022-07-08 10:26:46 +0200

    requestAnimationFrame

--- models/world.class.js
+++ models/world.class.js
@@ -8,14 +8,28 @@ class World {
     ];
 
     ctx;
+    canvas;
 
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
     }
 
 
     draw() {
+
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.height, this.character.width);
+        
+    
+        
+        let self = this;
+        requestAnimationFrame(function() {
+            self.draw();
+        });
     }
+
+    
 }
